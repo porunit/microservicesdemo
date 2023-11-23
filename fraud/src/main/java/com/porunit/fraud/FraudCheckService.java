f7c8073d00b39604a2073b0f4ce31e97446b989e
Vsevolod <shpakyn.26@gmail.com>
2023-11-23 17:24:09 +0300

    distribute tracing

--- fraud/src/main/java/com/porunit/fraud/FraudCheckService.java
+++ fraud/src/main/java/com/porunit/fraud/FraudCheckService.java
@@ -1,19 +1,22 @@
 package com.porunit.fraud;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FraudCheckService {
     private final FraudCheckHistoryRepository repository;
 
     public boolean isFraudulentCustomer(Integer customerId) {
+        log.info("New potential fraudster {}", customerId);
         repository.save(FraudCheckHistory.builder()
                         .isFraudster(false)
-                        .id(customerId)
+                        .customerId(customerId)
                         .createdAt(LocalDateTime.now())
                         .build());
         return false;
