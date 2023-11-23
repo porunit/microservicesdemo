1ab8d6d2add8d723f4c8fefe5ea446fd3ae614e0
Vsevolod <shpakyn.26@gmail.com>
2023-11-23 17:24:32 +0300

    ref

--- fraud/src/main/java/com/porunit/fraud/FraudApplication.java
+++ fraud/src/main/java/com/porunit/fraud/FraudApplication.java
@@ -9,6 +9,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class FraudApplication {
     public static void main(String[] args) {
-        SpringApplication.run(FraudApplication.class);
+        SpringApplication.run(FraudApplication.class,args);
     }
 }
